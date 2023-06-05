8af1c9f79bb33fb3df8313b35a3a4183d23c1059
farkdev <frk.finanzas.arg@gmail.com>
2023-06-05 05:46:52 -0300

    sacando comentario

--- source/routes/user.router.js
+++ source/routes/user.router.js
@@ -26,53 +26,53 @@ router.get('/',  async (req, res)=>{
 })
 
 //LOGIN DE USUARIO
-// router.post('/login', async (req,res)=>{
-//     let {email, password} = req.body
-//     email = email.trim()
-//     password = password.trim()
-//     if(!email || !password){
-//         return res.status(400).send({status: 'error', message: "Email y contraseña son obligatorios"})
-//     }
-//     let role = "user"
-//     if(email === "adminCoder@coder.com" && password === "adminCod3r123"){
-//         role = "admin"
-//     }
-//     const userDB = await userModel.findOne({email})
-//     if(!userDB) return res.status(404).json({status: "error", message: "Usuario o contraseña incorrecto"})
-//     if(!isValidPassword(password, userDB)) return res.status(401).send({status: "error", message: "el usuario o contraseña no es correcta"})
-//     req.session.user = {
-//         first_name: userDB.first_name,
-//         last_name: userDB.last_name,
-//         email: userDB.email,
-//         date_of_birth: userDB.date_of_birth,
-//         password: userDB.password,
-//         role: role
-//     }
-//     res.redirect('/')
-// })
+router.post('/login', async (req,res)=>{
+    let {email, password} = req.body
+    email = email.trim()
+    password = password.trim()
+    if(!email || !password){
+        return res.status(400).send({status: 'error', message: "Email y contraseña son obligatorios"})
+    }
+    let role = "user"
+    if(email === "adminCoder@coder.com" && password === "adminCod3r123"){
+        role = "admin"
+    }
+    const userDB = await userModel.findOne({email})
+    if(!userDB) return res.status(404).json({status: "error", message: "Usuario o contraseña incorrecto"})
+    if(!isValidPassword(password, userDB)) return res.status(401).send({status: "error", message: "el usuario o contraseña no es correcta"})
+    req.session.user = {
+        first_name: userDB.first_name,
+        last_name: userDB.last_name,
+        email: userDB.email,
+        date_of_birth: userDB.date_of_birth,
+        password: userDB.password,
+        role: role
+    }
+    res.redirect('/')
+})
 
 // REGISTRO DE USUARIO
-// router.post('/register', async (req, res)=>{
-//     try {
-//         const {first_name, last_name, email, date_of_birth, password }  = req.body
-//         const existUser = await userModel.findOne({email})
-//         if(existUser){ 
-//             return res.send({status:'error', mensaje: "El email ya se encuentra registrado"})
-//         }
-//         const newUser = {
-//             first_name, 
-//             last_name,
-//             email,
-//             date_of_birth,
-//             password: createHash(password),
-//             title: "Register"
-//         } 
-//         await userModel.create(newUser) 
-//         res.status(200).json({ status: 'success', message: 'Registro exitoso' });
-//     } catch (error) {
-//         res.status(500).json({ status: 'error', message: 'Error interno del servidor' });
-//     }
-// })
+router.post('/register', async (req, res)=>{
+    try {
+        const {first_name, last_name, email, date_of_birth, password }  = req.body
+        const existUser = await userModel.findOne({email})
+        if(existUser){ 
+            return res.send({status:'error', mensaje: "El email ya se encuentra registrado"})
+        }
+        const newUser = {
+            first_name, 
+            last_name,
+            email,
+            date_of_birth,
+            password: createHash(password),
+            title: "Register"
+        } 
+        await userModel.create(newUser) 
+        res.status(200).json({ status: 'success', message: 'Registro exitoso' });
+    } catch (error) {
+        res.status(500).json({ status: 'error', message: 'Error interno del servidor' });
+    }
+})
 
 
 
@@ -87,40 +87,6 @@ router.get('/',  async (req, res)=>{
 // LOGIN GITHUB
 
 
-// router.post('/login', passport.authenticate('login', {failureRedirect: '/faillogin'}), async (req,res)=>{
-//     if (!req.user) return res.status(401).send({status: 'error', message: "credenciales incorrectas"})
-//     req.session.user = {
-//         first_name: req.user.first_name,
-//         last_name: req.user.last_name, 
-//         email: req.user_email
-
-//     }
-//     res.send({status: 'success', message: "Iniciaste sesión"})
-// })
-
-
-
-// router.post('/register', passport.authenticate('register', {
-//     failureRedirect: '/failregister',
-// }), async (req,res)=>{
-//     res.send({status: 'success', message: "Usuario registrado"})
-// })
-
-// router.get('/github', passport.authenticate('github', {scope:['user:email']}))
-
-// router.get('/githubcallback',passport.authenticate('github',{failureRedirect:'/login'}), async(req,res)=>{
-//     req.session.user=req.user
-//     res.redirect('/')
-// })
-
-
-
-
-
-
-
-//LOGIN JWT
-
 router.post('/login', passport.authenticate('login', {failureRedirect: '/faillogin'}), async (req,res)=>{
     if (!req.user) return res.status(401).send({status: 'error', message: "credenciales incorrectas"})
     req.session.user = {
@@ -132,11 +98,59 @@ router.post('/login', passport.authenticate('login', {failureRedirect: '/faillog
     res.send({status: 'success', message: "Iniciaste sesión"})
 })
 
+
+
 router.post('/register', passport.authenticate('register', {
     failureRedirect: '/failregister',
 }), async (req,res)=>{
     res.send({status: 'success', message: "Usuario registrado"})
 })
+
+router.get('/github', passport.authenticate('github', {scope:['user:email']}))
+
+router.get('/githubcallback',passport.authenticate('github',{failureRedirect:'/login'}), async(req,res)=>{
+    req.session.user=req.user
+    res.redirect('/')
+})
+
+
+
+
+
+
+
+//LOGIN JWT
+
+// router.post('/login', async (req,res)=>{
+//         let {email, password} = req.body
+//         email = email.trim()
+//         password = password.trim()
+//         if(!email || !password){
+//             return res.status(400).send({status: 'error', message: "Email y contraseña son obligatorios"})
+//         }
+//         let role = "user"
+//         if(email === "adminCoder@coder.com" && password === "adminCod3r123"){
+//             role = "admin"
+//         }
+//         const userDB = await userModel.findOne({email})
+//         if(!userDB) return res.status(404).json({status: "error", message: "Usuario o contraseña incorrecto"})
+//         if(!isValidPassword(password, userDB)) return res.status(401).send({status: "error", message: "el usuario o contraseña no es correcta"})
+//         req.session.user = {
+//             first_name: userDB.first_name,
+//             last_name: userDB.last_name,
+//             email: userDB.email,
+//             date_of_birth: userDB.date_of_birth,
+//             password: userDB.password,
+//             role: role
+//         }
+//         res.redirect('/')
+//     })
+
+// router.post('/register', passport.authenticate('register', {
+//     failureRedirect: '/failregister',
+// }), async (req,res)=>{
+//     res.send({status: 'success', message: "Usuario registrado"})
+// })
 
 
 
